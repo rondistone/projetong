1695d72978c3a0a1160221ab84af9396f400c94d
rondinele <rondinele_loiola@oi.com.br>
2019-05-06 08:59:56 -0300

    06052019

--- src/app/cliente/add/add.component.ts
+++ src/app/cliente/add/add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente';
+import { ClienteService } from '../cliente.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-add',
@@ -9,15 +11,47 @@ import { Cliente } from '../cliente';
 export class AddComponent implements OnInit {
 
   private cliente: Cliente;
+  private clientes$: Observable<Cliente[]>;
 
-  constructor() { }
+  constructor(private clienteService: ClienteService) {
+  }
 
   ngOnInit() {
     this.cliente = new Cliente;
+    this.clientes$ = this.clienteService.getClientes();
+  }
+  
+
+  onSubmit(form) {
+    if (this.cliente.pws == form.value.conf) {
+      if (form.valid) {
+        this.clienteService.addCliente(this.cliente)
+        .subscribe(
+          res=>{
+            alert("cadastrado");
+        this.cliente = new Cliente;
+        form.reset();
+          },
+          err=>{
+            alert("erro ao cadastrar!");
+            console.log(err);
+          }
+        );
+        
+      }
+    }
+    this.clientes$ = this.clienteService.getClientes();
   }
 
-   onSubmit(form){
-    console.log(form);
-  
+  edit(id:number, cliente:Cliente){
+    console.log(id, cliente);
+    this.cliente = new Cliente;
+    // this.cliente = cliente;
+    this.cliente = this.clienteService.getcliente(id);
   }
+
+  remover(id:number){
+    this.clienteService.deleteCliente(id);
+  }
+
 }
---
+++ src/app/cliente/cliente.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {HttpClient}from '@angular/common/http';
+
+import{environment} from '../../environments/environment';
+import { Cliente } from './cliente';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  private clientes:Cliente[];
+  private url:string = environment.url_rest + "clientes";
+
+  constructor(
+    private http: HttpClient
+  ) { 
+    this.clientes = [];
+  }
+
+  public addCliente(cliente:Cliente){
+    //this.clientes.push(cliente);
+    return this.http.post(this.url, cliente);
+  }
+
+  public getClientes(){
+    //return this.clientes;
+    return this.http.get<Cliente[]>(this.url);
+  }
+
+  public updateCliente(id:number, cliente:Cliente){
+    //return this.clientes[id] = cliente;
+    return null;
+  }
+
+  public deleteCliente(id:number){
+    //this.clientes.splice(id, 1);
+    return null;
+  }
+
+  public getcliente(id:number){
+    //return this.clientes[id];
+    return null;
+  }
+
+}
